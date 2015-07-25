15487b60c72c3656e90812ceb554fe3614ce3f3b
林澍荣 <linshurong@kuanguang.local>
2015-07-25 11:34:24 +0800

    finish practice-22

--- ThinkingInJava/src/me/lsr/thinkinginjava/practice0509/MainClass.java
+++ ThinkingInJava/src/me/lsr/thinkinginjava/practice0509/MainClass.java
@@ -5,6 +5,28 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (RMB money : RMB.values()) {
+			switch (money) {
+			case ONE:
+				System.out.println("this is 1 yuan.");
+				break;
+			case FIVE:
+				System.out.println("this is 5 yuan.");
+				break;
+			case TEN:
+				System.out.println("this is 10 yuan.");
+				break;
+			case TWENTY:
+				System.out.println("this is 10 yuan.");
+				break;
+			case FIFTY:
+				System.out.println("this is 50 yuan.");
+				break;
+			case HUNDRED:
+				System.out.println("this is 100 yuan.");
+				break;
+			default:
+				System.out.println("this is ???.");
+			}
 			System.out.println(money + " " + money.ordinal());
 		}
 	}
